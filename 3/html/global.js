753d1c8e177d5d2f91d786c9756696ec43f3cd89
soloscribblecreatives <34957574+soloscribblecreatives@users.noreply.github.com>
2023-04-21 23:10:28 +0530

    Changes

--- 3/html/global.js
+++ 3/html/global.js
@@ -347,7 +347,7 @@ currentSlide();
 var selectedContentPath='';
 switch(pg_id){
 	case 1:
-	content='<link rel="stylesheet" type="text/css" href="slide1/slide1.css" media="screen"/><div class="background"><img src="slide1/1.jpg" width="1024" height="768" alt=""></div>';
+	content='<link rel="stylesheet" type="text/css" href="slide1/slide1.css" media="screen"/><div class="background"><img src="slide1/1.jpg" width="1024" height="768" alt=""></div><div class="popup"><img src="slide1/2.jpg" width="1024" height="768" alt=""></div><div class="pop_open" onclick="pop_open()"></div><div class="pop_close" onclick="pop_close()"></div><div class="popup1"><img src="slide1/3.jpg" width="1024" height="768" alt=""></div><div class="pop_open1" onclick="pop_open1()"></div><div class="pop_close1" onclick="pop_close1()"></div>';
 	break;
 
 }
@@ -489,3 +489,29 @@ $(document).ready(function(){
 		$('.touchbtn').css("display","block");
 	})
 })
+
+//----------------------------------//
+
+function pop_open() {
+	$('.popup').css('display','block');
+	$('.pop_open').css('display','none');
+	$('.pop_close').css('display','block');
+}
+
+function pop_close() {
+	$('.popup').css('display','none');
+	$('.pop_open').css('display','block');
+	$('.pop_close').css('display','none');
+}
+
+function pop_open1() {
+	$('.popup1').css('display','block');
+	$('.pop_open1').css('display','none');
+	$('.pop_close1').css('display','block');
+}
+
+function pop_close1() {
+	$('.popup1').css('display','none');
+	$('.pop_open1').css('display','block');
+	$('.pop_close1').css('display','none');
+}
